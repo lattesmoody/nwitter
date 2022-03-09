33f9ef92c65f1df00e6ff503df64c8da9d9a675a
lattesmoody <lattesmoody@gmail.com>
2022-03-09 12:07:01 +0900

    0309_save

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import App from './components/App.js';
+//import firebase from "./firebase";
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,4 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-//reportWebVitals();
