1239418a52c9cf234b812d58981e85bd41aa500b
lattesmoody <lattesmoody@gmail.com>
2022-03-09 15:08:42 +0900

    0309 1.2 Router Setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
-import React, {useState} from "react";
+import React from "react";
 import AppRouter from "./Router";
 
 function App() {
-  const [isLoggedIn, setIsLoggedin] = useState(false);
-  return <AppRouter isLoggedIn={isLoggedIn}/>;
+  return <AppRouter/>
 }
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
 
 import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
 const AppRouter =  () => {
+    const [isLoggedIn, setIsLoggedin] = useState(true);
     return (
         <Router>
             <Routes>
