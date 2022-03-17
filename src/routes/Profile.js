a00200b311a42e1ae1d5290e41c3a10ca6c807dc
lattesmoody <lattesmoody@gmail.com>
2022-03-17 18:37:18 +0900

    5.0

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,25 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import { getDocs,collection,query,where, orderBy } from "firebase/firestore";
+import React, { useEffect } from "react";
 
-export default () => {
+export default ({userObj}) => {
     const onLogOutClick = () => authService.signOut();
+
+    const getMyPosts = async() =>{
+        const q = query(collection(dbService,"posts"),
+        where("creatorId", "==", userObj.uid)
+        ,orderBy("createdAt")
+        );
+
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc)=>{
+            console.log(doc.id,"=> ", doc.data());
+        });
+    };
+    useEffect(()=>{
+        getMyPosts();
+    },[])
+
     return ( <>
     <button onClick= {onLogOutClick}>Log Out</button>
     </>
